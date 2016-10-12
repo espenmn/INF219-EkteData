4133969926bf1ac6548a802d2758bc67c2029660
Andre Dyrstad <ady006@student.uib.no>
2016-10-12 18:27:13 +0200

    Tried jquery

--- public/javascripts/formatting.js
+++ public/javascripts/formatting.js
@@ -134,6 +134,13 @@ function testcase(string){
     result = string;
 }*/
 
+
+var hostLink = 'http://localhost:3000';
+
+$.get(hostLink + "/api/text", function (res) {
+   input = res;
+});
+
 var input;
 
 var allDataValues = "{ startdatetime: 2016-09-15T20:00:28.000Z, timeseries:[ { temp: 17.8787291667 }, { temp: 16.8668440367 }, { temp: 15.4650091743 }, { temp: 14.6223030303 }, { temp: 14.3738181818 }, { temp: 14.1253333333 }, { temp: 13.8866373057 }, { temp: 13.8539948187 }, { temp: 13.4504 }, { temp: 12.7268166667 }, { temp: 11.8898148148 }, { temp: 10.6083333333 }, { temp: 9.5824657534 }, { temp: 9.1783561644 }, { temp: 8.7742465753 }, { temp: 8.2516126126 }, { temp: 8.1074684685 }, { temp: 7.9633243243 } ] }{ startdatetime: 2016-09-15T18:00:34.000Z, timeseries:[ { temp: 16.7343783784 }, { temp: 16.0443333333 }, { temp: 15.3462112676 }, { temp: 14.7318059701 }, { temp: 14.4090447761 }, { temp: 14.0862835821 }, { temp: 13.8604 }, { temp: 13.8044 }, { temp: 13.0767169811 }, { temp: 12.1122653061 }, { temp: 11.1411333333 }, { temp: 10.0235142857 }, { temp: 9.2813367698 }, { temp: 8.9706838488 }, { temp: 8.6600309278 }, { temp: 8.2112345133 }, { temp: 8.1240663717 } ] }{ startdatetime: 2016-09-15T16:00:43.000Z, timeseries:[ { temp: 16.7715882353 }, { temp: 15.6177272727 }, { temp: 14.9965151515 }, { temp: 14.4480666667 }, { temp: 14.2733047619 }, { temp: 14.06976 }, { temp: 13.88201 }, { temp: 13.7168666667 }, { temp: 13.3235333333 }, { temp: 12.3038285714 }, { temp: 11.471648 }, { temp: 10.669248 }, { temp: 9.0997173913 }, { temp: 8.8200493274 }, { temp: 8.4635470852 }, { temp: 8.0872 } ] }";
@@ -151,7 +158,7 @@ removeElements();
 
 function removeElements(){
 
-    input = dayly;
+    //input = dayly;
 
     input = input.replace(/\s/g, "");
     input = input.replace(/{/g, "");
