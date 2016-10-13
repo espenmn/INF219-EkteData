9ea4e49c44397ce3a9af8a84f0dbd8cb18a54e11
Andre Dyrstad <ady006@student.uib.no>
2016-10-13 19:27:59 +0200

    Moved format to api

--- routes/api.js
+++ routes/api.js
@@ -31,6 +31,7 @@ router.get('/allData', function (req, res, next) {
             if (doc != null) {
                 console.log(doc);
                 queryToBeSavedAsText += stringify(doc, {pretty: true, space: 1})
+                removeElements(queryToBeSavedAsText);
             } else {
                 callback();
             }
@@ -265,5 +266,89 @@ router.get('/averageYear', function (req, res, next) {
 
     res.send();
 });
+
+//////////////////////////
+var dateList = [];
+var dataList = [];
+var depthList = [];
+
+var parameter = "";
+
+var list;
+
+function removeElements(input){
+
+    input = input.replace(/\s/g, "");
+    input = input.replace(/{/g, "");
+    input = input.replace(/}/g, "");
+
+    list = input.split(/,|_id:|timeseries:\[|\]/);
+
+    /*if(list[0].indexOf("startdatetime") !== -1)
+     parameter = list[2].substring(0,list[2].indexOf(":"));*/
+
+    return addToList();
+
+}
+
+function addToList() {
+
+    var date = "";
+
+    for (var i = 0; i < list.length; i++) {
+
+        if(list[i].indexOf("average") !== -1 || list[i].indexOf("value") !== -1) {
+            dataList.push(parseFloat(list[i].substring(list[i].indexOf(":") + 1)).toFixed(3));
+            dateList.push(date.slice(0,-1));
+            date = "";
+        }
+        else if(list[i].indexOf("depth") !== -1){
+            if(!(list[i] in depthList))
+                depthList.push(list[i].substring(list[i].indexOf(":") + 1) + "m");
+        }
+        /*else if(list[i].indexOf("startdatetime") !== -1){
+         list[i] = list[i].replace(/-|T/g, ".");
+         list[i] = list[i].replace(/startdatetime:/g, "");
+         dateList.push(list[i].slice(0,-11))
+         }*/ else if(list[i] === ""){
+
+        } /*else if(list[i].indexOf(parameter) !== -1){
+         dataList.push(parseFloat(list[i].substring(list[i].indexOf(":") + 1)).toFixed(3));
+         }*/
+        else {
+            date += list[i].substring(list[i].indexOf(":") + 1) + ".";
+        }
+    }
+    return buildString();
+}
+
+function buildString() {
+
+    var finalString = "";
+
+    for(var i=0;i<dateList.length;i++){
+
+        if(i == 0)
+            finalString += "Tid            Nr   ";
+        else
+            finalString += dateList[i] + "   " + (i) + "    ";
+
+        for(var j=0;j<depthList.length;j++) {
+
+            if(i == 0)
+                finalString += depthList[j] + "      ";
+            else
+                finalString += dataList[j] + "      ";
+
+        }
+        finalString += "\n";
+    }
+    console.log(finalString);
+    return finalString;
+}
+
+/////////////////
+
+
 
 module.exports = router;
